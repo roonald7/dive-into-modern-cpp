7467e2442e67fd714d8b3c5f3b182c33c94e2f9c
roonald7 <roonald.anderson017@gmail.com>
2026-06-21 15:33:52 -0300

    Phase 1: Finalize Modules and nested directory structure

---
+++ phase-1/modules/src/Math.cppm
@@ -0,0 +1,11 @@
+export module Math;
+
+export namespace Math {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ phase-1/modules/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+import Math;
+
+int main() {
+    std::cout << "--- Modules: Basic Example ---\n";
+    std::cout << " 5 + 10 = " << Math::add(5, 10) << "\n";
+    std::cout << " 5 * 10 = " << Math::multiply(5, 10) << "\n";
+    return 0;
+}
